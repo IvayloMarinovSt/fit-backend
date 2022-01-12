af5a844a41a57206cc652ce4823348faeec841ab
Ivaylo Marinov <marinov.ivaylo@fh-swf.de>
2022-01-12 19:09:12 +0100

    category get by id endpoint

--- src/main/java/de/fhswf/fit/service/CategoryController.java
+++ src/main/java/de/fhswf/fit/service/CategoryController.java
@@ -1,31 +1,31 @@
 package de.fhswf.fit.service;
 
 import de.fhswf.fit.model.Category;
-import de.fhswf.fit.model.Product;
 import de.fhswf.fit.repositories.CategoryRepository;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.HEAD;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-
 import java.util.List;
 
 @Path("/category")
-@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.TEXT_HTML})
-@Produces({MediaType.APPLICATION_JSON})
+@Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+      MediaType.TEXT_HTML })
+@Produces({ MediaType.APPLICATION_JSON })
 public class CategoryController
 {
     @Inject
     CategoryRepository categoryRepository;
 
     @GET
-    public Response getAll(){
+    public Response getAll()
+    {
         try
         {
             List<Category> categories = categoryRepository.findAll();
@@ -34,13 +34,41 @@ public class CategoryController
                 return Response.noContent().build();
             }
             return Response.ok(categories).build();
-        }catch (Exception exc){
+        }
+        catch (Exception exc)
+        {
+            return Response.serverError().build();
+        }
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getById(@PathParam("id") Long id)
+    {
+        try
+        {
+            Category category = categoryRepository.findById(id);
+            if (category == null)
+                return Response.noContent().build();
+            return Response.ok().build();
+        }
+        catch (Exception e)
+        {
             return Response.serverError().build();
         }
     }
 
     @POST
-    public Category add(Category category){
-        return categoryRepository.update(category);
+    public Response add(Category category)
+    {
+        try
+        {
+            return Response.ok(categoryRepository.update(category)).build();
+        }
+        catch (Exception e)
+        {
+            return Response.serverError().build();
+        }
     }
+
 }
